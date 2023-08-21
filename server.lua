555aef8088bd2c659999a30acdb43023e3cfd9f7
Benjamin <136846661+insery@users.noreply.github.com>
2023-08-21 14:53:01 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,5 +1,3 @@
-local logEnabled = false
-
 RegisterServerEvent('3dme:shareDisplay')
 AddEventHandler('3dme:shareDisplay', function(text, type)
 	TriggerClientEvent('3dme:triggerDisplay', -1, text, source, type)
